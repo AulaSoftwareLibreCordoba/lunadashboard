40c9d977dd39aeacf4c0f341f93804eb090839ad
Álvaro Fernández Amaro <alvaroofernaandez@gmail.com>
2024-10-11 16:34:41 +0200

    inicialización componente sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import SideBar from './components/SideBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+    <html lang="es" class="antialiased dark:bg-gray-950 w-screen h-screen">
+      <SideBar />
+    </html>
     </>
   )
 }
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,11 @@
+export default function SideBar() {
+  return (
+    <>
+    <div className="grid md:w-80 md:border-r-2 md:border-[#1f2937] md:h-screen ">
+      <h1 className="m-8 text-xl font-bold text-center text-white">
+        <img src="/luna.png" alt="Logo luna" className="mx-auto size-14 md:size-10"/>
+      </h1>
+    </div>
+    </>
+  )
+}
